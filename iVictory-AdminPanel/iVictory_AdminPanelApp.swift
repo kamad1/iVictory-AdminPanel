cfa7b8098fc38a9ce1aacba2ee2e866a8e27ee1d
KaMaDi <artkamadi@gmail.com>
2024-07-21 13:08:25 +0300

    Initial Commit

---
+++ iVictory-AdminPanel/iVictory_AdminPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iVictory_AdminPanelApp.swift
+//  iVictory-AdminPanel
+//
+//  Created by Jedi on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct iVictory_AdminPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
